c76098021672bed88c186686ebee23ddab10cfcc
Zach Mowrey <zbmowrey@gmail.com>
2021-10-19 22:27:17 -0400

    Add custom error response to CDN
    Without this, we'll never redirect back to index.html, thus breaking
    the SPA.

--- .github/workflows/terraform/main.tf
+++ .github/workflows/terraform/main.tf
@@ -73,6 +73,19 @@ resource "aws_cloudfront_distribution" "web-dist" {
 
   aliases = [local.app_domain, join(".", ["www", local.app_domain])]
 
+  custom_error_response {
+    error_code = 404
+    response_code = 200
+    response_page_path = "/index.html"
+    error_caching_min_ttl = 30
+  }
+  custom_error_response {
+    error_code = 403
+    response_code = 200
+    response_page_path = "/index.html"
+    error_caching_min_ttl = 30
+  }
+
   default_cache_behavior {
     allowed_methods        = ["GET", "HEAD", "OPTIONS"]
     cached_methods         = ["HEAD", "GET", "OPTIONS"]
