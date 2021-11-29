ca2f680c93efd1f656020862cf67e94f64fc1c4c
Zach Mowrey <zbmowrey@gmail.com>
2021-11-29 14:22:36 -0500

    Add TF CHIP certification

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row py-3 mb-2">
+  <div class="row py-3 mb-1">
     <div class="col-md-7 order-2">
       <div class="overflow-hidden">
         <h2 class="text-color-dark font-weight-bold text-12 mb-2 pt-0 mt-0"
@@ -25,28 +25,26 @@
         <a href="https://www.credly.com/badges/a5a7870c-1ec1-47bf-8a91-8a637436e31d"
            target="_blank"><img height="100" src="img/certs/aws-saa.png"
                                 alt="AWS Certified Solutions Architect Associate"></a>
-        <a href="https://www.credly.com/badges/9b8e2cb1-2aa3-434e-b194-500a642893f6"
-           target="_blank"><img height="100" src="img/certs/aws-ccp.png"
-                                alt="AWS Certified Cloud Practitioner"></a>
         <a href="https://www.credly.com/badges/431e3f52-42c8-4fa5-894d-98201887d13b"
            target="_blank"><img height="100" src="img/certs/tf-associate.png" alt="Terraform Associate"></a>
+        <a href="https://www.credly.com/badges/918fe1af-7b9d-449c-a389-c8455922114b"
+           target="_blank"><img height="100" src="img/certs/tf-chip.png" alt="Certified HashiCorp Implementation Partner"></a>
 
       </p>
-      <p class="pb-3" data-appear-animation="fadeInUpShorter"
+      <p class="pb-1" data-appear-animation="fadeInUpShorter"
          data-appear-animation-delay="800">
-        In my current role, I create and maintain Cloud Economics Best-Practices guides and support a
+        In my current role, I create and maintain Cloud Economics Best-Practice guides and support a
         team of analysts in gathering data, formulating strategies, and presenting findings. If you'd
         like to join <a target="_blank" href="https://2ndwatch.com/careers">2nd Watch</a>,
         please feel free to ask for a referral!
       </p>
-      <p class="pb-3" data-appear-animation="fadeInUpShorter"
-         data-appear-animation-delay="800">
+      <p class="pb-1" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="800">
         I welcome all connection requests on <a target="_blank"
                                                 href="https://linkedin.com/in/zbmowrey/">LinkedIn</a>.
         Some of my code is available on <a target="_blank" href="https://github.com/zbmowrey">Github</a>.
       </p>
     </div>
-    <div class="col-md-5 order-md-2 mb-4 mb-lg-0"
+    <div class="col-md-5 order-md-2 mb-2 mb-lg-0"
          data-appear-animation="fadeInRightShorter">
       <img src="img/team/zach.png" class="img-fluid mb-2" alt="">
     </div>
