8ab88ed18dc7dc56299401a44f1e308b63479bbe
Zach Mowrey <zbmowrey@gmail.com>
2021-11-29 21:59:59 -0500

    Mostly formatting changes to Portfolio

--- src/components/DevOps.vue
+++ src/components/DevOps.vue
@@ -13,11 +13,11 @@
         All of my sites use a common workflow and set of tools for Version Control and Deployment. This is how they
         integrate:
       </p>
-      <p class="text-center">
+      <p class="text-center" id="version-control">
         <img src="img/devops-workflow.png" alt="DevOps Workflow Diagram">
       </p>
-      <div class="overflow-hidden" id="version-control">
-        <h4 class="text-color-dark font-weight-bold mb-2 pt-0 mt-0">Version Control in my Version Control</h4>
+      <div class="overflow-hidden">
+        <h4 class="text-color-dark font-weight-bold mb-2 pt-0 mt-0">Version Control</h4>
       </div>
       <p>
         I use this
@@ -27,13 +27,13 @@
         secrets/credentials. For those, there is unfortunately no good solution except to trust that Github and/or
         Terraform will treat my secrets with utmost care.
       </p>
-      <p>
+      <p class="pb-3">
         If I were an enterprise, I'd subscribe to Terraform Cloud Enterprise and self-host TCE and Gitlab, using Vault
         for secrets management. Sadly, I do not have an enterprise budget. Moving forward, my plan is to implement some
         form of credential rotation and use the Github, Terraform, and Serverless APIs to update the AWS credentials
         frequently.
       </p>
-      <div class="overflow-hidden">
+      <div class="overflow-hidden" id="deployment-site">
         <h4 class="text-color-dark font-weight-bold mb-2 pt-0 mt-0">Deployment: Site Code</h4>
       </div>
       <p class="pb-3">
@@ -44,7 +44,7 @@
           Site Deployment Github Action
         </a>.
       </p>
-      <div class="overflow-hidden">
+      <div class="overflow-hidden" id="deployment-lambda">
         <h4 class="text-color-dark font-weight-bold mb-2 pt-0 mt-0">Deployment: Lambda</h4>
       </div>
       <p>
@@ -53,29 +53,29 @@
         Organizations to limit the blast radius of potential issues). Because of this high monthly cost for my extremely
         low change volume, I've elected to keep the Serverless handling within Github Actions. This serves admirably.
       </p>
-      <p>
+      <p class="pb-3">
         If you review my <a href="https://github.com/zbmowrey/zbmowrey-com/blob/develop/.github/workflows/serverless-mail-handler.yml" target="_blank">Serverless Deployment Github Action</a>, you'll notice I install a tagging module - that allows me to ensure that all resources are tagged appropriately.
         This is key to being able to allocate costs to the application or team responsible.
       </p>
-      <div class="overflow-hidden">
+      <div class="overflow-hidden" id="deployment-infra">
         <h4 class="text-color-dark font-weight-bold mb-2 pt-0 mt-0">Deployment: Infrastructure</h4>
       </div>
-      <p>
+      <p class="pb-3">
         I use Terraform to define my Infrastructure-as-Code and I use Terraform Cloud to deploy infrastructure changes
         from key branches of my repositories: develop, staging, and main. Terraform Cloud is configured and managed by
         the <a href="https://github.com/zbmowrey/terraform-cloud" target="_blank">terraform-cloud</a> repository. This
         keeps all of my organizations, workspaces, settings, and variables under version control.
       </p>
-          <div class="overflow-hidden">
+          <div class="overflow-hidden" id="cloud-governance">
         <h4 class="text-color-dark font-weight-bold mb-2 pt-0 mt-0">Cloud Governance</h4>
       </div>
-      <p>
+      <p class="pb-3">
         If you're not using multiple AWS accounts, today is the best day to start. They're free and they give you
         a world of opportunity to partition services. I use this <a href="https://github.com/zbmowrey/cloud-admin">
         Cloud Governance Github Repository</a> to maintain a root account, organization, organizational units,
         application environment accounts, service control policies, and centralized billing/reporting.
       </p>
-      <div class="overflow-hidden">
+      <div class="overflow-hidden" id="cloud-accounts">
         <h4 class="text-color-dark font-weight-bold mb-2 pt-0 mt-0">Cloud Account Partitioning</h4>
       </div>
       <p>
