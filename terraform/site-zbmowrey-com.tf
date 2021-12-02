f3afc6cd7d33f49a61a156e00b43fed91baee801
Zach Mowrey <zbmowrey@gmail.com>
2021-12-02 13:21:27 -0500

    Remove sid property. Getting invalid policy json again.

--- terraform/site-zbmowrey-com.tf
+++ terraform/site-zbmowrey-com.tf
@@ -53,7 +53,6 @@ resource "aws_s3_bucket" "web-primary" {
     Version   = "2012-10-17"
     Statement = [
       {
-        Sid       = "OAIRead"
         Effect    = "Allow"
         Principal = [aws_cloudfront_origin_access_identity.web-oai.iam_arn]
         Action    = ["s3:GetObject", "s3:GetObjectVersion"]
@@ -94,7 +93,6 @@ resource "aws_s3_bucket" "web-secondary" {
     Version   = "2012-10-17"
     Statement = [
       {
-        Sid       = "OAIRead"
         Effect    = "Allow"
         Principal = [aws_cloudfront_origin_access_identity.web-oai.iam_arn]
         Action    = ["s3:GetObject", "s3:GetObjectVersion"]
@@ -214,6 +212,7 @@ resource "aws_cloudfront_distribution" "web-dist" {
   }
 
   viewer_certificate {
+    cloudfront_default_certificate = false
     acm_certificate_arn            = aws_acm_certificate.web-cert.arn
     ssl_support_method             = "sni-only"
   }
