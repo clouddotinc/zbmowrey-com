5f5f106df91aa9e02276a22f2de43f8128ac1dd0
Zach Mowrey <zbmowrey@gmail.com>
2021-12-02 15:12:55 -0500

    Wrap arn in quotes. Bad JSON.

--- terraform/site-zbmowrey-com.tf
+++ terraform/site-zbmowrey-com.tf
@@ -56,7 +56,7 @@ resource "aws_s3_bucket" "web-primary" {
     {
       "Sid": "PublicRead",
       "Effect": "Allow",
-      "Principal": [${aws_cloudfront_origin_access_identity.web-oai.iam_arn}],
+      "Principal": ["${aws_cloudfront_origin_access_identity.web-oai.iam_arn}"],
       "Action": ["s3:GetObject", "s3:GetObjectVersion"],
       "Resource": ["arn:aws:s3:::${local.web_primary_bucket}/*"]
     }
