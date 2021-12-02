82b0800e66b07e3c39cd23600e2b873244f0e4a2
Zach Mowrey <zbmowrey@gmail.com>
2021-12-02 15:14:27 -0500

    Testing HEREDOC without SID

--- terraform/site-zbmowrey-com.tf
+++ terraform/site-zbmowrey-com.tf
@@ -54,7 +54,6 @@ resource "aws_s3_bucket" "web-primary" {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Sid": "PublicRead",
       "Effect": "Allow",
       "Principal": ["${aws_cloudfront_origin_access_identity.web-oai.iam_arn}"],
       "Action": ["s3:GetObject", "s3:GetObjectVersion"],
